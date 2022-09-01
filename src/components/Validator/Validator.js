bbd1d24b71d2460da79a1ee6fd77b51e3866532a
Nika <nikaantidze4@gmail.com>
2022-09-01 15:36:45 +0400

    feat: add laptop form and product list component

--- src/components/Validator/Validator.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Validator() {
-  const emailValidation = (email) => {
-    if (email.includes("@redberry.ge")) return true;
-    else return false;
-  };
-  return <div></div>;
-}
